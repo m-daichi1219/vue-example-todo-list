10a60332fe77114ba218082a787f97716d5775ea
m-daichi1219 <matu9525@gmail.com>
2019-08-02 11:27:17 +0900

    メイン画面の追加

--- src/components/templates/TaskMainView.vue
+++ src/components/templates/TaskMainView.vue
@@ -1,3 +1,59 @@
 <template>
-  <p>TaskMainView</p>
+  <div>
+    <label>期限の近い順に並べる</label>
+    <input
+      type="checkbox"
+      id="isNear"
+      v-model="isNear"
+    >
+    <label>タグを指定</label>
+    <select name="taglist">
+      <option
+        v-for="(tag, index) in tagList"
+        :key="index"
+        :id="index"
+      >
+        {{tag}}
+      </option>
+    </select>
+    <Button
+      type="add"
+    >
+    アイテムの追加
+    </Button>
+    <p>TaskMainView</p>
+      <div v-if="tasks">
+        <TaskList 
+          :tasks="tasks"
+        >
+        </TaskList>
+      </div>
+  </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Button from '@/components/atoms/Button.vue'
+import TaskList from '@/components/organisms/TaskList.vue'
+
+export default {
+  name: 'TaskMainView',
+  components: {
+    Button,
+    TaskList
+  },
+  data () {
+    return {
+      isNear: false,
+      selectedTag: ''
+    }
+  },
+  computed: {
+    ...mapGetters({
+      tasks: 'fetchTasks',
+      tagList: 'fetchTagList'
+    })
+  }
+}
+</script>
+
--- tests/unit/specs/templates/TaskMainView.spec.js
+++ tests/unit/specs/templates/TaskMainView.spec.js
@@ -1,65 +1,65 @@
 import { expect } from 'chai'
 import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import TaskMainView from '@/components/templates/TaskMainView.vue'
 
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
 describe('TaskMainView.vue', () => {
-  let taskMainView
-  beforeEach(() => {
-    taskMainView = mount(TaskMainView, {
-      propsData: {
-        tasks: [
-          {
-            name: 'TaskName1',
-            tags: ['tag1', 'tag2'],
-            detail: 'detail comment',
-            limit: '2019-12-01T12:59',
-            id: 1
-          },
-          {
-            name: 'TaskName2',
-            tags: ['tag2', 'tag3'],
-            detail: 'detail comment',
-            limit: '2019-12-15T12:59',
-            id: 2
-          },
-          {
-            name: 'TaskName3',
-            tags: ['tag3', 'tag4'],
-            detail: 'detail comment',
-            limit: '2019-12-31T12:59',
-            id: 3
-          }
-        ]
-      }
-    })
-  })
+  const tasks = [
+    {
+      name: 'TaskName1',
+      tags: ['tag1', 'tag2'],
+      detail: 'detail comment',
+      limit: '2019-12-01T12:59',
+      id: 1
+    },
+    {
+      name: 'TaskName2',
+      tags: ['tag2', 'tag3'],
+      detail: 'detail comment',
+      limit: '2017-12-15T12:59',
+      id: 2
+    },
+    {
+      name: 'TaskName3',
+      tags: ['tag3', 'tag4'],
+      detail: 'detail comment',
+      limit: '2018-12-31T12:59',
+      id: 3
+    }
+  ]
+
+  const tagList = ['tag1', 'tag2', 'tag3', 'tag4']
 
   describe('プロパティ', () => {
-    describe('tasks', () => {
-      describe('デフォルト', () => {
-        // TODO: expect
+    describe('tagList', () => {
+      let getters
+      let store
+
+      beforeEach(() => {
+        getters = {
+          fetchTasks: () => tasks,
+          fetchTagList: () => tagList
+        }
+
+        store = new Vuex.Store({
+          getters
+        })
       })
 
-      describe('期限が近い順チェックボックスが入力されている場合', () => {
-        // TODO: expect
-      })
+      it('タグリストのドロップダウンが表示されていること', () => {
+        const testView = mount(TaskMainView, {
+          localVue,
+          store
+        })
 
-      describe('タグが指定されている場合', () => {
-        // TODO: expect
-      })
-    })
-  })
-
-  describe('イベント', () => {
-    describe('タスクの追加', () => {
-      it('タスク作成モーダルを表示', () => {
-        // TODO: expect
-      })
-    })
-
-    describe('タスクの更新', () => {
-      it('タスク更新モーダルを表示', () => {
-        // TODO: expect
+        expect(testView.find('select').findAll('option').length).to.equal(tagList.length)
+        expect(testView.find('select').findAll('option').at(0).text()).to.equal(tagList[0])
+        expect(testView.find('select').findAll('option').at(1).text()).to.equal(tagList[1])
+        expect(testView.find('select').findAll('option').at(2).text()).to.equal(tagList[2])
+        expect(testView.find('select').findAll('option').at(3).text()).to.equal(tagList[3])
       })
     })
   })
